7f0bcf44219e93d96cbd913024567566037e1c8c
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-10-05 10:49:39 +0500

    firebase

---
+++ app/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+ https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnNALsXUlGsoOgWHEXleM6d1PbTGB46Y8",
+  authDomain: "esxpense-tracker.firebaseapp.com",
+  projectId: "esxpense-tracker",
+  storageBucket: "esxpense-tracker.appspot.com",
+  messagingSenderId: "17404366665",
+  appId: "1:17404366665:web:f4570b4dfe2820faa21498"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react"
 
 export default function Home() {
 
-  const [item, setItem] = useState([
+  const [items, setItem] = useState([
     { name: 'Coffee', price: 4.56 },
     { name: 'Movie', price: 24.56 },
     { name: 'Candy', price: 74.56 },
@@ -20,6 +20,24 @@ export default function Home() {
             <input className='col-span-2 p-3 mx-3 border' type="text" placeholder='Enter $' />
             <button className='text-white bg-slate-950 hover:bg-slate-900 p-3 textxl' type="submit">+</button>
           </form>
+          <ul>
+            {items.map((item, id)=>(
+              <li key={id} className="my-4 w-full flex justify-between bg-slate-950">
+                <div className="p-4 w-full flex justify-between">
+                  <span className="capitalize">{item.name}</span>
+                  <span>${item.price}</span>
+                </div>
+                <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">X</button>
+              </li>
+            ))}
+          </ul>
+          {items.length < 1 ? (''): (
+            <div className="flex   justify-between p-3" >
+              <span>Total</span>
+              <span>${total}</span>
+
+            </div>
+          )}
         </div>
       </div>
     </main>
