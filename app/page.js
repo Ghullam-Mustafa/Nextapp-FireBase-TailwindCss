0acef819366265aa32dd5e3c5a4279b79f8cfbef
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-10-06 11:36:00 +0500

    add data

--- app/page.js
+++ app/page.js
@@ -1,24 +1,44 @@
 'use client'
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase"; 
 
 export default function Home() {
 
-  const [items, setItem] = useState([
-    { name: 'Coffee', price: 4.56 },
-    { name: 'Movie', price: 24.56 },
-    { name: 'Candy', price: 74.56 },
+  const [items, setItems] = useState([
+    // { name: 'Coffee', price: 4.56 },
+    // { name: 'Movie', price: 24.56 },
+    // { name: 'Candy', price: 74.56 },
   ])
-
+  const [newItem, setNewItem] = useState({name:'' , price:''})
   const [total, setTotal] = useState(0);
+
+  // Add item for data base 
+  const addItem = async (e) =>{
+    e.preventDefault();
+    if(newItem.name !== '' && newItem.price !== ''){
+      //  setItems([...items,newItem])'
+      // await addDoc(collection(db, 'items'),{
+      //   name:newItem.name.trim(),
+      //   price: newItem.price
+      // }
+       await addDoc(collection(db, 'items'),{
+        name:newItem.name.trim(),
+        price: newItem.price,
+      })
+      setNewItem( {name:'', price: ''})
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm ">
         <h1 className='text-4xl p-4 text-center'>Expense Tracker</h1>
         <div className='bg-slate-800 p-4 rounded-lg'>
           <form className='grid grid-cols-6 items-center  text-black'>
-            <input className='col-span-3 p-3 border' type="text" placeholder='Enter Item' />
-            <input className='col-span-2 p-3 mx-3 border' type="text" placeholder='Enter $' />
-            <button className='text-white bg-slate-950 hover:bg-slate-900 p-3 textxl' type="submit">+</button>
+            <input value={newItem.name} onChange={(e) => setNewItem({...newItem, name: e.target.value})} className='col-span-3 p-3 border' type="text" placeholder='Enter Item' />
+            <input value={newItem.price} onChange={(e) => setNewItem({...newItem, price: e.target.value})} className='col-span-2 p-3 mx-3 border' type="text" placeholder='Enter $' />
+            <button onClick={addItem} className='text-white bg-slate-950 hover:bg-slate-900 p-3 textxl' type="submit">+</button>
           </form>
           <ul>
             {items.map((item, id)=>(
