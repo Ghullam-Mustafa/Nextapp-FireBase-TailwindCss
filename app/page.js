1f8e7b0c06c0f96b3aa87634e136273eef169337
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-10-07 12:43:57 +0500

    project complete

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc , getDoc , querySnapshot,deleteDoc, query, onSnapshot, doc} from "firebase/firestore";
 import { db } from "./firebase"; 
 
 export default function Home() {
@@ -28,6 +28,31 @@ export default function Home() {
       })
       setNewItem( {name:'', price: ''})
     }
+  };
+
+  // Read item  from database
+  useEffect(() =>{
+    const  q = query(collection(db, "items"));
+    const unsubscribe = onSnapshot(q , (querySnapshot)=>{
+      let itemsArr = []; 
+       querySnapshot.forEach((doc) => {
+        itemsArr.push({...doc.data(), id: doc.id})
+        });
+        setItems(itemsArr);
+
+        const calculateTotal = () => {
+          const totalPrice = itemsArr.reduce((sum,item)=>sum  + parseFloat(item.price),0)
+          setTotal(totalPrice);
+        }
+        calculateTotal()
+        return () =>unsubscribe()
+
+    })
+  },[])
+
+  // Delete item from database  
+  const deleteItem = async (id) =>{
+    await deleteDoc(doc(db,'items',id))
   }
 
   return (
@@ -47,7 +72,7 @@ export default function Home() {
                   <span className="capitalize">{item.name}</span>
                   <span>${item.price}</span>
                 </div>
-                <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">X</button>
+                <button onClick={()=> deleteItem(item.id)} className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">X</button>
               </li>
             ))}
           </ul>
